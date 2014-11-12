5e570f3466b781f6183f18d72602b82409bfbec8
thigg <thilo@kogge.net>
2014-11-12 12:31:40 +0200

    try

---
+++ citavi/citavi.4.4/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'citavi' 
+$installerType = 'MSI' 
+$url = 'http://citavi.com/msi-setup'
+$silentArgs = '/quiet'
+$validExitCodes = @(0) 
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
