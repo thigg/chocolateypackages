ddb55edb380c56911a1d9265314f677d9c0d0592
thigg <thilo@kogge.net>
2014-11-15 14:26:37 +0200

    jabref added

---
+++ jabref/jabref/tools/chocolateyInstall.ps1
@@ -0,0 +1,3 @@
+﻿$url = 'http://downloads.sourceforge.net/project/jabref/jabref/2.10/JabRef-2.10-setup.exe?r=&ts=1416053772&use_mirror=heanet'
+$validExitCodes = @(0) 
+Install-ChocolateyPackage "JabRef" "EXE" "/S" "$url"  -validExitCodes $validExitCodes
