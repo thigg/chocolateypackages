b925b04ee102d47c9ae80ccf20cbb104c14c520f
thigg <thilo@kogge.net>
2015-03-18 21:25:30 +0100

    0.9.9

--- eclipse/eclipse-java-luna.4.4.2/package/tools/chocolateyInstall.ps1
+++ eclipse/eclipse-java-luna.4.4.2/package/tools/chocolateyInstall.ps1
@@ -2,5 +2,6 @@
 $url64 = 'http://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/luna/R/eclipse-java-luna-R-win32-x86_64.zip&r=1&protocol=http'
 $packageName = 'eclipse-java-luna' 
 Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" "$url64"
-$target = Join-Path $MyInvocation.MyCommand.Definition "eclipse.exe"
-Install-ChocolateyDesktopLink $target
+$target = "%ChocolateyInstall%\lib\eclipse\eclipse.exe"
+$desktop  = [Environment]::GetFolderPath("Desktop")
+Install-ChocolateyShortcut -targetPath "$target" -shortcutFilePath "desktop/eclipse.lnk"
